497590f41257710ac6fc68cd31b943febb00c6f7
Wesley Ferreira <37143147+WesleyIsFerreira@users.noreply.github.com>
2021-02-26 19:54:29 -0300

    Adicionando cards

--- resources/assets/my-app/src/app/app.component.ts
+++ resources/assets/my-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Post } from './post';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  public posts: Post[] = [
+    new Post("Wesley", "Meu post", "Coisa de loco bixo", "wdias9122@gmail.com", "Esse dia foi épico"),
+    new Post("Maria", "Meu post 2", "Oloquinho meu", "maria@gmail.com", "Esse dia foi loco"),
+    new Post("Ricardo", "Meu post 3", "Coisa de loco bixo", "ricardo@gmail.com", "Esse dia foi xato")
+  ]
 }
--- resources/assets/my-app/src/app/app.module.ts
+++ resources/assets/my-app/src/app/app.module.ts
@@ -2,17 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+
+
 
 import { AppComponent } from './app.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule,
+    MatDividerModule,
+    MatIconModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
