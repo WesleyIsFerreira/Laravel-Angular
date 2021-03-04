818aad4765fb2e2866e4f2349c4577edc338215e
Wesley Ferreira <37143147+WesleyIsFerreira@users.noreply.github.com>
2021-03-04 00:01:38 -0300

    View parcialmente finalizado

--- resources/assets/my-app/src/app/app.component.ts
+++ resources/assets/my-app/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { Post } from './post';
+import { PostDialogComponent } from './post-dialog/post-dialog.component';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +13,31 @@ export class AppComponent {
   public posts: Post[] = [
     new Post("Wesley", "Meu post", "Coisa de loco bixo", "wdias9122@gmail.com", "Esse dia foi épico"),
     new Post("Maria", "Meu post 2", "Oloquinho meu", "maria@gmail.com", "Esse dia foi loco"),
+    new Post("Wesley", "Meu post", "Coisa de loco bixo", "wdias9122@gmail.com", "Esse dia foi épico"),
+    new Post("Maria", "Meu post 2", "Oloquinho meu", "maria@gmail.com", "Esse dia foi loco"),
+    new Post("Wesley", "Meu post", "Coisa de loco bixo", "wdias9122@gmail.com", "Esse dia foi épico"),
+    new Post("Maria", "Meu post 2", "Oloquinho meu", "maria@gmail.com", "Esse dia foi loco"),
+    new Post("Wesley", "Meu post", "Coisa de loco bixo", "wdias9122@gmail.com", "Esse dia foi épico"),
+    new Post("Maria", "Meu post 2", "Oloquinho meu", "maria@gmail.com", "Esse dia foi loco"),
     new Post("Ricardo", "Meu post 3", "Coisa de loco bixo", "ricardo@gmail.com", "Esse dia foi xato")
-  ]
+  ];
+
+  constructor(public dialog: MatDialog){
+
+  }
+
+  openDialog(){
+    const dialogRef = this.dialog.open(PostDialogComponent, {
+      width: '600px'
+    });
+
+    dialogRef.afterClosed().subscribe(
+      (result) => {
+        if (result) {
+          console.log(result);
+        }
+      }
+    );
+
+  }
 }
--- resources/assets/my-app/src/app/app.module.ts
+++ resources/assets/my-app/src/app/app.module.ts
@@ -6,27 +6,41 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatIconModule} from '@angular/material/icon';
 import {MatCardModule} from '@angular/material/card';
+import { FlexLayoutModule } from "@angular/flex-layout";
+import {MatInputModule} from '@angular/material/input';
+import {MatDialogModule} from '@angular/material/dialog';
+
 
 
 
 import { AppComponent } from './app.component';
 import { PostComponent } from './post/post.component';
+import { PostDialogComponent } from './post-dialog/post-dialog.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostComponent
+    PostComponent,
+    PostDialogComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     BrowserAnimationsModule,
     MatButtonModule,
     MatToolbarModule,
     MatDividerModule,
     MatIconModule,
-    MatCardModule
+    MatCardModule,
+    FlexLayoutModule,
+    MatInputModule,
+    MatDialogModule
   ],
   providers: [],
+  entryComponents : [
+    PostDialogComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
